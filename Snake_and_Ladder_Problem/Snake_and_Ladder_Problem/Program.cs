cb9f8db0fb3d18fcdbf330a48abde3818684b129
virendra <virendrachouhan1494@gmail.com>
2022-07-30 22:42:40 +0530

    UC1_Implimented

---
+++ Snake_and_Ladder_Problem/Snake_and_Ladder_Problem/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Snake_and_Ladder_Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int Player_One_Position = 0;
+            Console.WriteLine("Position of Player 1 is at " + Player_One_Position);
+        }
+    }
+}
