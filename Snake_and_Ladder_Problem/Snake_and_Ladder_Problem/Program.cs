e21ffe7a05dd2e42c65a066edb9453e91f00c2e4
virendra <virendrachouhan1494@gmail.com>
2022-07-30 23:24:16 +0530

    UC4

--- Snake_and_Ladder_Problem/Snake_and_Ladder_Problem/Program.cs
+++ Snake_and_Ladder_Problem/Snake_and_Ladder_Problem/Program.cs
@@ -6,13 +6,43 @@ namespace Snake_and_Ladder_Problem
     {
         static void Main(string[] args)
         {
-            
+            const int NO_PLAY = 1;
+            const int LADDER = 2;
+            const int SNAKE = 3;
+
             int Player_One_Position = 0;
             Console.WriteLine("Position of Player 1 is at " + Player_One_Position);
+            while (Player_One_Position < 100)
+            {
 
-            Random random = new Random();
-            int rolls = random.Next(1, 6);
-            Console.WriteLine("Player 1 rolls the die " + rolls);
+
+                Random random = new Random();
+                int rolls = random.Next(1, 6);
+                Console.WriteLine("Player 1 rolls the die " + rolls);
+
+
+                int check = random.Next(1, 4);
+                switch (check)
+                {
+                    case NO_PLAY:
+                        Player_One_Position = Player_One_Position + 0;
+                        break;
+                    case LADDER:
+                        Player_One_Position = Player_One_Position + rolls;
+                        break;
+                    case SNAKE:
+                        Player_One_Position = Player_One_Position - rolls;
+                        
+
+                    if (Player_One_Position <= 0)
+                        {
+                            Player_One_Position = 0;
+                        }
+                        break;
+                }
+
+                Console.WriteLine("Player Position is:" + Player_One_Position);
+            }
         }
     }
 }
